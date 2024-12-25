02a57c8c97cc2278de43a29769c2ad601fd24e35
jeongwwon <spwldmlgla@naver.com>
2024-12-25 16:05:52 +0900

    JPA

---
+++ src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package hello.itemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
